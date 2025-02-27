dcc71d872f8cc8b5c1574954fc99e4ff9b83a1d8
Isaac Phiri <isaacphiri315@gmail.com>
2025-02-27 02:50:35 +0200

    Add Development Services component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "@/components/navbar/page";
 import Hero from "@/components/hero/page";
+import DevelopmentServices from "@/components/development-services/page";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <DevelopmentServices />
     </>
   );
 }
